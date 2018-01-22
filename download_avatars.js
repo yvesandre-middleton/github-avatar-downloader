204f91c548b358e7f34bf421a18bd73fae343fcf
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-01-22 22:37:40 +0000

    Step 3: Create getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
