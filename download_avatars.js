d99a283b4d241f0db603ff1c49f687f7673f2053
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-01-23 00:01:23 +0000

    got them usernames :)

--- download_avatars.js
+++ download_avatars.js
@@ -6,7 +6,6 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
  var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
    headers: {
@@ -21,6 +20,11 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+  var data = JSON.parse(result);
+   // console.log(data);
+  for (i of data) {
+    console.log(i.avatar_url)
+  }
  console.log("Errors:", err);
- console.log("Result:", result);
+ // console.log("Result:", result);
 });
